ccb4227acb328edfd96b79966ded37f2f05dda07
MuhammadZaeem500 <zaeemsheikh102@gmail.com>
2025-08-09 05:02:00 +0500

    Minor Changes

--- src/components/core/BannerSection.js
+++ src/components/core/BannerSection.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from "react";
+import { Box } from "@mui/material";
 
 const BannerSection = () => {
-
   return (
     <Box
       sx={{
-        height: '100vh',
-        backgroundColor: '#000',
-        position: 'relative',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
+        height: "100vh",
+        backgroundColor: "#000",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: "hidden",
         padding: { xs: 2, sm: 4, md: 6 },
-        marginTop: '-100px'
+        marginTop: { xs: "-300px", md: "-100px" },
       }}
     >
       <Box
         component="img"
-        src="/images/div.png" 
+        src="/images/div.png"
         alt="Banner Image"
         sx={{
-          maxWidth: '100%',
-          maxHeight: '100%',
-          objectFit: 'contain',
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
+          maxWidth: "100%",
+          maxHeight: "100%",
+          objectFit: "contain",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
         }}
       />
     </Box>
